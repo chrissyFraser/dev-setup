e4c2ddba0b287819596b7799a1839af4d2ed3408
Chrissy Fraser <plum5152@gmail.com>
2023-06-27 11:32:50 -0700

    Header, Footer, Logo

---
+++ app-1/src/components/Logo/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './radicalx-logo.jpeg';
+
+function Logo() {
+    return (
+        <div>
+            <img src={logo} alt="logo" />
+        </div>
+    );
+}
+
+export default Logo;
